2ff991a667f6457f21178e9c5b4a04077b0132ae
Dvl <84147332+Sylbird@users.noreply.github.com>
2024-03-19 15:42:39 -0500

    Reverting all previous commits to commit

--- app/src/main/java/com/app/notes/MainActivity.java
+++ app/src/main/java/com/app/notes/MainActivity.java
@@ -10,16 +10,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -31,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     private int NightMode;
     private Realm realm;
     private SharedPreferences sharedPreferences;
-    private final String TAG = this.getClass().getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,13 +52,13 @@ public class MainActivity extends AppCompatActivity {
 
         listNotes.addChangeListener(notes -> noteAdapter.notifyDataSetChanged());
 
-        // Get NightMode from SharedPreferences if it exists
+        // Night Mode
         sharedPreferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
         NightMode = sharedPreferences.getInt("NightModeInt", 1);
         AppCompatDelegate.setDefaultNightMode(NightMode);
 
-        // Switch Night Mode
         SwitchCompat switchBtn = findViewById(R.id.switch_dayNight);
+
         switchBtn.setOnCheckedChangeListener((compoundButton, isChecked) -> {
             if (isChecked){
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
@@ -75,11 +66,10 @@ public class MainActivity extends AppCompatActivity {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
         });
-        // Check if Night Mode is ON
+
         boolean isNightModeOn = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES;
         switchBtn.setChecked(isNightModeOn);
 
-        // Adding Listeners for Buttons
         FloatingActionButton addTaskButton = findViewById(R.id.addTaskButton);
         addTaskButton.setOnClickListener(v -> {
             // Create intent to start NoteCreationActivity
@@ -87,42 +77,14 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        FloatingActionButton syncJsonButton = findViewById(R.id.syncJsonButton);
-        syncJsonButton.setOnClickListener(v -> {
-            HttpService.get("http://10.0.2.2:3000/Notes", response -> runOnUiThread(() -> {
-
-                // Replace the existing notes with the new ones from the JSON response
-                realm.executeTransactionAsync(realm -> {
-                    // Convert the JSON response to a list of Note objects
-                    Type listType = new TypeToken<ArrayList<Note>>(){}.getType();
-
-                    List<Note> notes = new Gson().fromJson(response, listType);
-
-                    // Clear the existing notes
-                    realm.delete(Note.class);
-
-                    // Add the notes from the JSON response
-                    for (Note note : notes) {
-                        realm.insert(note);
-                    }
-                });
-                Toast.makeText(MainActivity.this,"Sincronizacion Exitosa!.",Toast.LENGTH_SHORT).show();
-            }));
-        });
-
-        ImageButton logoutButton = findViewById(R.id.logoutButton);
-        logoutButton.setOnClickListener(v -> {
-            logout();
-        });
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        // Get NightMode from AppCompatDelegate
         NightMode = AppCompatDelegate.getDefaultNightMode();
-        // Save NightMode to SharedPreferences
+
         sharedPreferences = getSharedPreferences("SharedPrefs", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
@@ -132,31 +94,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void addInitialNote() {
         // Check if the note already exists
-        if (realm.where(Note.class).equalTo("id", 0).findFirst() == null) {
+        if (realm.where(Note.class).equalTo("id", 1).findFirst() == null) {
             // Start a Realm transaction
             realm.executeTransactionAsync(realm -> {
                 // Create a new note
-                Note initialNote = realm.createObject(Note.class,0);
+                Note initialNote = realm.createObject(Note.class, 1);
                 initialNote.setTitle("Sample Title");
                 initialNote.setContent("This is a sample note content.");
                 initialNote.setCreatedTime(1678828800000L);
             });
         }
-    }
-
-    private void logout() {
-        // Acceder a SharedPreferences y eliminar la información de inicio de sesión
-        SharedPreferences sharedPreferences = getSharedPreferences("UserPrefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.remove("username");
-        editor.remove("password");
-        editor.putBoolean("isLoggedIn", false); // Actualizar el estado de inicio de sesión
-        editor.apply();
-
-        // Redirigir a LoginActivity
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
     }
     @Override
     protected void onDestroy() {
